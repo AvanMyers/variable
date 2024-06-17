f456a5a5126535d30288a03c4c072873fa52477c
AvanMyers <smail1341@gmail.com>
2024-06-17 15:50:31 +0300

    Добавьте файлы проекта.

---
+++ ДЗ/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ДЗ
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int integer = 8;
+            string s = "Hi";
+            bool isTrue = false;
+            float f = 0.5f;
+            double d = 0.0;
+            long l = 12412515125;
+            char c = 'a';
+            bool isFalce = true;
+            int i = 1678;
+            string number = "Nine";
+        }
+    }
+}
