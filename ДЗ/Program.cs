805aed57e2bc0225b1f3d792d6fd74e9f6987fff
AvanMyers <smail1341@gmail.com>
2024-06-24 15:24:45 +0300

    Update Program.cs

--- ДЗ/Program.cs
+++ ДЗ/Program.cs
@@ -11,13 +11,13 @@ namespace ДЗ
         static void Main(string[] args)
         {
             int integer = 8;
-            string s = "Hi";
-            bool isTrue = false;
+            string offer = "Hi";
+            bool isOpen = false;
             float f = 0.5f;
             double d = 0.0;
             long l = 12412515125;
             char c = 'a';
-            bool isFalce = true;
+            bool isClosed = true;
             int i = 1678;
             string number = "Nine";
         }
